63e29383c3eeb279f9c689210761316313c92622
Taher Shabbiri <tahershabbiri@gmail.com>
2026-07-11 17:14:43 -0400

    Add Firebase configuration to firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,3 +1,11 @@
 
 // Paste your Firebase config here
-const firebaseConfig = {};
+const firebaseConfig = {
+    apiKey: "AIzaSyC8px49gIrlr9BT6HEOEN37dc3a5x7t_zo",
+    authDomain: "fmb-ezzi-85d65.firebaseapp.com",
+    projectId: "fmb-ezzi-85d65",
+    storageBucket: "fmb-ezzi-85d65.firebasestorage.app",
+    messagingSenderId: "331248092002",
+    appId: "1:331248092002:web:5a26a3fd1d40dd08c8e23b",
+    measurementId: "G-0FDCCDPDLT"
+};
